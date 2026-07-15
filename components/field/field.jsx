ffe5382cf55851651f6d6e5b7c32eee01b7e77a4
rheydachnik-cyber <rheydachnik@gmail.com>
2026-07-15 13:14:56 +0400

    Create field.jsx

---
+++ components/field/field.jsx
@@ -0,0 +1,11 @@
+import { forwardRef } from 'react';
+import styles from './field.module.css';
+
+export const Field = forwardRef(({ error, ...props }, ref) => {
+	return (
+		<div>
+			<input ref={ref} {...props} />
+			{error && <span className={styles.errorLabel}>{error}</span>}
+		</div>
+	);
+});
